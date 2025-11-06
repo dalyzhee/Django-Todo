2888bee35ab42a117104c9b2588e6dea7623b501
dalyzhee <dalmasodhiambolevi@gmail.com>
2025-11-06 12:45:27 +0300

    fourth commit - hidden secret key

--- TodoApp/TodoApp/settings.py
+++ TodoApp/TodoApp/settings.py
@@ -19,11 +19,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-!p^*kiysp=n!b+e%-d=cwuk0ul7#j8fq5tq9su^r!5)q0_9+4)'
+from decouple import config
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+SECRET_KEY = config('SECRET_KEY')
+DEBUG = config('DEBUG', default=False, cast=bool)
 
 ALLOWED_HOSTS = []
 
