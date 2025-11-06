5759f5e3edddcbaf24115e79ce561de1ad707131
dalyzhee <dalmasodhiambolevi@gmail.com>
2025-11-06 03:35:49 +0300

    Second commit after creating models, views and urls admin panel

--- TodoApp/Todo/models.py
+++ TodoApp/Todo/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+class Todo(models.Model):
+    # Todo fields
+    title = models.CharField(max_length=200)
+    completed = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    
+    # arragement based on recently created
+    class Meta:
+        ordering = ['-created_at']
+    
+    def __str__(self):
+        return self.title
+    
--- TodoApp/Todo/views.py
+++ TodoApp/Todo/views.py
@@ -1,3 +1,38 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
+from .models import Todo
 
-# Create your views here.
+# Function based view
+def todo_list(request):
+    todos = Todo.objects.all()
+    context = {'todos': todos}
+    return render(request, 'Todo/todo_list.html', context)
+
+# Class based views
+class TodoListView(ListView):
+    model = Todo
+    template_name = 'Todo/todo_list.html'
+    context_object_name = Todo
+    
+class TodoCreateView(CreateView):
+    model = Todo
+    template_name = 'Todo/todo_create.html'
+    success_url = reverse_lazy('todo-list')
+
+class TodoUpdateView(UpdateView):
+    model = Todo
+    template_name = 'Todo/todo_update.html'
+    success_url = reverse_lazy('todo-list')
+
+class TodoDeleteView(DeleteView):
+    model = Todo
+    template_name = 'Todo/todo_delete.html'
+    success_url = reverse_lazy('todo-list')
+
+# Function for toggling completed
+def toggle_todo(request, pk):
+    todo = get_object_or_404(Todo, pk=pk)
+    todo.completed = not todo.completed
+    todo.save()
+    return redirect('todo-list')
